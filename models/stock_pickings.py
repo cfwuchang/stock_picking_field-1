df8d406aaa138728d370fad9296486a53b164be5
cfwuchang <67139173+cfwuchang@users.noreply.github.com>
2022-05-26 08:39:48 +0800

    Update stock_pickings.py

--- models/stock_pickings.py
+++ models/stock_pickings.py
@@ -14,15 +14,16 @@ class StockPickings(models.Model):
     #             if i.name in self.origin:
     #                 # self.x_origin=i.product_id.name
     #                 self.write({'x_origin':i.product_id.name})
-    @api.constrains('scheduled_date')
+    
     @api.onchange('x_origin')
+    @api.constrains('scheduled_date')
     def get_origin(self):
-
         att_model = self.env['mrp.production']
         query = [("state","!=","draft"),("state","!=","cancel"),("state","!=","done")]
         for i in att_model.search(query):
             try:
                 if i.name in self.origin:
+                    # self.x_origin=i.product_id.name
                     self.write({'x_origin':i.product_id.name})
             except:
                 pass
